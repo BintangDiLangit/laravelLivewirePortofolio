a96d6576fdbc902f8c8b95c463ea9582a4c6f386
bintangdilangit <syekhuda463@gmail.com>
2021-06-29 18:01:45 +0700

    update / create masih bentrok

---
+++ README.md
@@ -0,0 +1,20 @@
+* composer require laravel/jetstream
+
+* php artisan jetstream:install livewire --teams
+
+* npm install && npm run dev
+
+
+
+* php artisan storage:link
+
+- change app_url in env => APP_URL=http://127.0.0.1:8000
+
+
+- list command jetstream livewire
+- will be generate views in laravel
+* php artisan vendor:publish --tag=jetstream-views
+
+
+- make livewire
+* php artisan make:livewire main
---
+++ app/Http/Livewire/Porto.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Livewire;
+use App\Models\Portofolio;
+use Livewire\WithFileUploads;
+use Livewire\Component;
+
+class Porto extends Component
+{
+    use WithFileUploads;
+
+    public $portofolio, $portofolios, $title, $description, $image, $portofolio_id;
+
+    public $isModal = false, $showModal = false;
+
+    public function render()
+    {
+        $this->portofolios = Portofolio::all();
+        return view('livewire.porto');
+    }
+
+    // show modal
+    public function openModal(){
+        $this->isModal = true;
+    }
+
+    // close modal
+    public function closeModal(){
+        $this->isModal = false ;
+    }
+
+    public function closeShowModal(){
+        $this->showModal = false ;
+    }
+
+    public function openShowModal(){
+        $this->showModal = true ;
+    }
+
+    public function edit($id){
+        $this->portofolio = Portofolio::find($id);
+        $this->title = $this->portofolio->title;
+        $this->description = $this->portofolio->description;
+        $this->openModal();
+    }
+
+    public function resetFields(){
+        $this->title = '';
+        $this->description = '';
+        $this->image = '';
+    }
+
+    public function store(){
+        $this->validate([
+            'title' => 'required|string|max:28|min:5',
+            'description' => 'required|string|max:255',
+            'image' => 'required|image',
+        ]);
+
+        Portofolio::updateOrCreate(['id' => $this->portofolio_id],[
+            'title' => $this->title,
+            'description' => $this->description,
+            'portofolio_image' => $this->image->storePublicly('portofolio-image','public'),
+        ]);
+
+        session()->flash('message', $this->portofolio_id ? $this->title.' berhasil di update' : 'Sukses membuat portofolio');
+        $this->closeModal();
+        $this->resetFields();
+    }
+
+    public function show($id){
+        $this->portofolio = Portofolio::find($id);
+        $this->openShowModal();
+    }
+
+    public function delete($id){
+        Portofolio::destroy($id);
+        session()->flash('message', 'Sukses menghapus portofolio');
+        $this->closeShowModal();
+    }
+
+
+}
